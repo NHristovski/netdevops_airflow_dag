caf0c1255a8b49c66de6c8a5e7ab82b352b7f799
Nikola Hristovski <nikola.hristovski@netcetera.com>
2025-12-01 19:55:24 +0100

    [no issue] Create the second resource

--- import_data_to_maat.py
+++ import_data_to_maat.py
@@ -39,43 +39,6 @@ def import_data_to_maat():
         resource_id='srlinux-leaf1'
     )
 
-    # # Task to find or handle missing resource
-    # @task
-    # def retrieve_resource(**context):
-    #     """
-    #     Try to find a resource, handle 404 errors gracefully.
-    #     """
-    #     from operators.maat_api_operator import MaatResourceOperator
-    #
-    #     # Create an instance of the operator
-    #     operator = MaatResourceOperator(
-    #         task_id='find_resource_op',
-    #         operation='retrieve',
-    #         resource_id='srlinux-leaf1',
-    #     )
-    #
-    #     # Execute the operator
-    #     result = operator.execute(context)
-    #
-    #     # Pull the HTTP status code from XCom (pushed by the operator in the same task context)
-    #     ti = context['ti']
-    #     http_status_code = ti.xcom_pull(key='http_status_code')
-    #
-    #     print(f"HTTP Status Code: {http_status_code}")
-    #     print(f"Result: {result}")
-    #
-    #     # Check if it's a 404 error (resource not found)
-    #     if http_status_code == 404:
-    #         return {'status': 'not_found', 'resource_id': 'srlinux-leaf1', 'http_status_code': http_status_code}
-    #
-    #     # Return the result with status code
-    #     if isinstance(result, dict):
-    #         result['http_status_code'] = http_status_code
-    #
-    #     return result
-    #
-    # find_resource = retrieve_resource()
-
     # Branching task to decide whether to create resource or skip
     @task.branch
     def check_resource_status(**context):
@@ -86,8 +49,6 @@ def import_data_to_maat():
         """
         ti = context['ti']
         result = ti.xcom_pull(task_ids='retrieve_resource')
-
-        print(f'The result after xcom_pull: {result}')
 
         # Get HTTP status code from the result dictionary
         http_status_code = result.get('http_status_code') if result else None
@@ -111,7 +72,6 @@ def import_data_to_maat():
         resource_data={
             "category": "device.router",
             "description": "Nokia SRLinux Router - leaf1",
-            "id": "srlinux-leaf1",
             "name": "srlinux-leaf1",
             "serialNumber": "SRL-7220-IXR-D2L-2024-001",
             "resourceCharacteristic": [
@@ -142,6 +102,71 @@ def import_data_to_maat():
 
     )
 
+    retrieve_second_resource = MaatResourceOperator(
+        task_id='retrieve_second_resource',
+        operation='retrieve',
+        resource_id='srlinux-leaf2'
+    )
+
+    @task.branch
+    def check_second_resource_status(**context):
+        """
+        Check if resource was found or not (404).
+        If 404, proceed to create_resource.
+        Otherwise, skip to end_task.
+        """
+        ti = context['ti']
+        result = ti.xcom_pull(task_ids='retrieve_second_resource')
+
+        http_status_code = result.get('http_status_code') if result else None
+
+        print(f'HTTP Status Code: {http_status_code}')
+
+        # Check if it's a 404 error
+        if http_status_code == 404:
+            print("Resource not found (404), will create it")
+            return 'create_second_resource'
+        else:
+            print("Resource found, skipping creation")
+            return 'skip_creation'
+
+    second_check_status = check_second_resource_status()
+
+    create_second_resource = MaatResourceOperator(
+        task_id='create_resource',
+        operation='create',
+        resource_data={
+            "category": "device.router",
+            "description": "Nokia SRLinux Router - leaf2",
+            "name": "srlinux-leaf2",
+            "serialNumber": "SRL-7220-IXR-D2L-2024-002",
+            "resourceCharacteristic": [
+                {
+                    "name": "vendor",
+                    "value": "Nokia"
+                },
+                {
+                    "name": "model",
+                    "value": "7220 IXR-D2L"
+                },
+                {
+                    "name": "os-version",
+                    "value": "24.10.1"
+                },
+                {
+                    "name": "management-ip",
+                    "value": "172.80.80.12"
+                },
+                {
+                    "name": "site",
+                    "value": "Site-B"
+                },
+            ],
+            "@type": "PhysicalResource",
+            "@schemaLocation": "https://bitbucket.software.geant.org/projects/OSSBSS/repos/maat-schema/raw/TMF639-ResourceInventory-v4-pionier.json"
+        }
+    )
+
     # Empty task for the skip branch
     @task
     def skip_creation():
@@ -153,10 +178,18 @@ def import_data_to_maat():
 
     skip_task = skip_creation()
 
+
+
     # Define task dependencies
     list_resources >> retrieve_resource >> check_status
     check_status >> create_resource
     check_status >> skip_task
+
+
+    create_resource >> retrieve_second_resource >> check_second_resource_status
+
+    check_second_resource_status >> create_second_resource
+    check_second_resource_status >> skip_task
 
 # Instantiate the DAG
 dag_instance = import_data_to_maat()
